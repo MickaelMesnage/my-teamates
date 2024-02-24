1a4ede5c18881ef8b268e0566bf05b78f7a43cca
Mickael <mickael.mesnage@lonestone.io>
2024-02-24 15:49:08 +0100

    learn about revalidate

--- src/components/organisms/TeamDeleteButton/TeamDeleteAction.ts
+++ src/components/organisms/TeamDeleteButton/TeamDeleteAction.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { urls } from "@/urls";
+import { revalidatePath } from "next/cache";
 
 export async function teamDeleteAction(userId: string) {
   await prisma.team.delete({
@@ -8,5 +10,8 @@ export async function teamDeleteAction(userId: string) {
       id: userId,
     },
   });
+
   await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  revalidatePath(urls.teams.list);
 }
