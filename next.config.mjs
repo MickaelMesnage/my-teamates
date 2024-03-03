ffddd2407fd52f3cb50ad183d6cc68fd3b4746c3
Mickael <mickael.mesnage@lonestone.io>
2024-03-03 23:50:18 +0100

    redirect home

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+      // {
+      //   source: "/login",
+      //   destination: "/signin",
+      //   permanent: true,
+      // },
+    ];
+  },
+};
 
 export default nextConfig;
